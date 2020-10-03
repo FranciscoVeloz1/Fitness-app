8ac795e897969b69226462880a9211b1c7de4082
Francisco Veloz <franciscoveloz245@gmail.com>
2020-10-02 19:17:02 -0500

    We programed the index.js and import the database key

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//Dependencies
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const mysql = require('mysql')
+const myconnection = require('express-myconnection')
+
+//Initialization
+const app = express()
+
+//Importing database key
+const database = require('./database/keys.js')
+
+//Importing routes
+//Example const cardRoutes = require('./routes/cards')
+
+//Settings
+app.set('port', process.env.PORT || 4000)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//Middleware
+app.use(morgan('dev'))
+app.use(myconnection(mysql, database, 'single'))
+app.use(express.urlencoded({ extended: false }))
+
+//Routes
+//Example app.use('/', cardRoutes);
+
+//Static Files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Starting server
+app.listen(app.get('port'), () => {
+    console.log('Server on port 4000')
+})
