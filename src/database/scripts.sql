96160701f97fb2b3b58ddad54cdc6b21f0babb0b
Francisco Veloz <franciscoveloz245@gmail.com>
2020-10-03 12:03:14 -0500

    We change the table peso and show de information in the app

--- src/database/scripts.sql
+++ src/database/scripts.sql
@@ -9,6 +9,7 @@ create table peso(
     id int primary key auto_increment,
     fecha varchar(50),
     distancia double,
+    tiempo int,
     calorias double,
     peso double
 );
@@ -16,4 +17,4 @@ create table peso(
 describe peso;
 
 --Insert one info
-insert into peso values(null, '03/10/2020', 1.1, 18.6, 0);
+insert into peso values(null, '03/10/2020', 1.1, 20, 18.6, 0);
