63a90db1f38e365756f9c2c7600f2e71a71102cf
Francisco Veloz <franciscoveloz245@gmail.com>
2020-10-11 12:48:09 -0500

    Transformamos mainController a una clase

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,28 +1,32 @@
-const controller = {}
+class Controller {
 
-controller.list = (req, res) => {
-    req.getConnection((err, conn) => {
-        conn.query('select * from peso', (err, row) => {
-            if(err) {
-                res.json(err)
-            }
-
-            res.render('index', {
-                data: row
+    List = (req, res) => {
+        req.getConnection((err, conn) => {
+            conn.query('select * from peso', (err, row) => {
+                if(err) {
+                    res.json(err)
+                }
+    
+                res.render('index', {
+                    data: row
+                })
             })
         })
-    })
-}
+    }
 
-controller.save = (req, res) => {
-    const data = req.body
-
-    req.getConnection((err, conn) => {
-        conn.query('insert into peso set ?', [data], (err, row) => {
-            console.log(row)
-            res.redirect('/')
+    Save = (req, res) => {
+        const data = req.body
+    
+        req.getConnection((err, conn) => {
+            conn.query('insert into peso set ?', [data], (err, row) => {
+                console.log(row)
+                res.redirect('/')
+            })
         })
-    })
+    }
+    
 }
+
+const controller = new Controller()
 
 module.exports = controller
